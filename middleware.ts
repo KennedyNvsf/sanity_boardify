ade9de64fc0f71cfb76d92805aa0cf447c7c71f0
Webstack-Corporation <webstackcorp@gmail.com>
2024-02-14 14:15:26 +0000

    boardify build completed

--- middleware.ts
+++ middleware.ts
@@ -11,16 +11,16 @@ const publicRoutes = ["/", "/studio", "/studio/structure", "/api/webhook"];
 export default authMiddleware({
   publicRoutes,
   afterAuth(auth, req) {
-    // if (auth.userId && auth.isPublicRoute) {
-    //   let path = "/select-org";
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
 
-    //   if (auth.orgId) {
-    //     path = `/organization/${auth.orgId}`;
-    //   }
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
 
-    //   const orgSelection = new URL(path, req.url);
-    //   return NextResponse.redirect(orgSelection);
-    // }
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
 
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
